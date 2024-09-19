271ac5fb2cbd43153cb5f7e35e00b7e5f17bd2d0
unknown <juan.vasquez-v@labinfo.is.escuelaing.edu.co>
2024-09-19 16:01:40 -0500

    Bono parcial

---
+++ README.md
@@ -0,0 +1,92 @@
+# Bono Parcial
+
+
+## Getting Started
+
+Download the project from [the repository.](https://github.com/Sebasvasquezz/ArepParcialBono)
+
+### Prerequisites
+
+* [Maven](https://maven.apache.org/): Automate and standardize the lifecycle of software construction.
+* [Git](https://www.git-scm.com/): Decentralized Configuration Manager.
+
+### Installing
+
+1. **Maven**
+    * Download Maven at http://maven.apache.org/download.html.
+    * You need to have Java installed (Java 17 or later recommended).
+    * Follow the instructions at http://maven.apache.org/download.html#Installation.
+
+2. **Git**
+    * Download Git at https://git-scm.com/download/win.
+    * Follow the instructions at https://git-scm.com/book/en/v2/Getting-Started-Installing-Git.
+
+### Running the Application
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/Sebasvasquezz/ArepParcialBono.git
+
+    cd your-repo-directory
+    ```
+
+2. Compile the proyect:
+    ```sh
+    mvn package
+    ```
+
+    Deberías ver una salida similar a esta:
+    ```sh
+    [INFO] Building jar: C:\Users\juan.vasquez-v.LABINFO\Desktop\bono\target\bono-1.0-SNAPSHOT.jar
+    [INFO] ------------------------------------------------------------------------
+    [INFO] BUILD SUCCESS
+    [INFO] ------------------------------------------------------------------------
+    [INFO] Total time:  6.834 s
+    [INFO] Finished at: 2024-09-19T15:51:09-05:00
+    [INFO] ------------------------------------------------------------------------
+    ```
+
+3.  Run the facade service in a terminal:
+    ```sh
+    java -cp target/bono-1.0-SNAPSHOT.jar edu.escuelaing.arep.CalcReflexFacade 
+    
+    o
+
+    & 'C:\Program Files\Java\jdk-1.8\bin\java.exe' '-cp' 'C:\Users\juan.vasquez-v.LABINFO\Desktop\bono\target\classes' 'edu.escuelaing.arep.CalcReflexFacade'
+    ```
+
+    When you run the application, you should see the following:
+    ![Execution CalcReflexFacade](images/execution1.png)
+    
+
+    Then run the calculator service in another terminal:
+
+     ```sh
+    java -cp target/bono-1.0-SNAPSHOT.jar edu.escuelaing.arep.CalcReflexBEServer 
+    
+    o
+
+    & 'C:\Program Files\Java\jdk-1.8\bin\java.exe' '-cp' 'C:\Users\juan.vasquez-v.LABINFO\Desktop\bono\target\classes' 'edu.escuelaing.arep.CalcReflexBEServer
+    ```
+
+   When you run the application, you should see the following:
+    ![Execution CalcReflexBEServer](images/execution2.png)
+
+    
+4. Now you can access the following [link](http://localhost:35000/calculadora):
+    ![Browser](images/browser.png)
+5. Enter a math or bbl method, click on calculate and you will get the result::
+    ![Result](images/result.png)
+
+## Built With
+
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [Git](http://git-scm.com/) - Version Control System
+
+## Authors
+
+* **Juan Sebastian Vasquez Vega**  - [Sebasvasquezz](https://github.com/Sebasvasquezz)
+
+## Date
+
+September 19, 2024
---
+++ src/main/java/edu/escuelaing/arep/CalcReflexFacade.java
@@ -0,0 +1,87 @@
+package edu.escuelaing.arep;
+
+import java.net.*;
+import java.io.*;
+
+public class CalcReflexFacade {
+    public static void main(String[] args) throws IOException {
+        int port = 35000;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Servidor de fachada escuchando en el puerto " + port);
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    String command = sacarComando(in);
+
+                    if (command != null) {
+                        String response = enviarSolicitudCalculo(command);
+                        enviarRespuesta(out, "application/json", response);
+                    } else {
+                        String clientHtml = obtenerCliente();
+                        enviarRespuesta(out, "text/html", clientHtml);
+                    }
+                }
+            }
+        }
+    }
+
+    private static String sacarComando(BufferedReader in) throws IOException {
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println("Recibí: " + line);
+            if (line.startsWith("GET /computar?comando=")) {
+                return line.split("=")[1].split(" ")[0];
+            }
+            if (!in.ready()) {
+                break;
+            }
+        }
+        return null;
+    }
+
+    private static String enviarSolicitudCalculo(String command) throws IOException {
+        URL calcServiceUrl = new URL("http://localhost:36000/compreflex?comando=" + command);
+        HttpURLConnection connection = (HttpURLConnection) calcServiceUrl.openConnection();
+        connection.setRequestMethod("GET");
+
+        try (BufferedReader calcIn = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            return calcIn.readLine();
+        }
+    }
+
+    private static void enviarRespuesta(PrintWriter out, String contentType, String body) {
+        out.println("HTTP/1.1 200 OK\r\n" + "Content-Type: " + contentType + "\r\n\r\n" + body);
+    }
+
+    public static String obtenerCliente() {
+        return "<!DOCTYPE html>\n"
+        + "<html>\n"
+        + "<head>\n"
+        + "<meta charset=\"UTF-8\">\n"
+        + "<title>Calculadora Reflexiva</title>\n"
+        + "</head>\n"
+        + "<body>\n"
+        + "<h1>Calculadora Reflexiva</h1>\n"
+        + "<form>\n"
+        + "<label for=\"comando\">Ingrese el Comando deseado ej: sin(30), bbl(50, 2, 6, 12, 5) :</label><br>\n"
+        + "<input type=\"text\" id=\"comando\" name=\"comando\" placeholder=\"Comando (param1, ...):\"><br><br>\n"
+        + "<input type=\"button\" value=\"Calcular\" onclick=\"loadGetMsg()\">\n"
+        + "</form>\n"
+        + "<div id=\"getrespmsg\"></div>\n"
+        + "<script>\n"
+        + "function loadGetMsg() {\n"
+        + "let comando = document.getElementById(\"comando\").value;\n"
+        + "const xhttp = new XMLHttpRequest();\n"
+        + "xhttp.onload = function() {\n"
+        + "document.getElementById(\"getrespmsg\").innerHTML = this.responseText;\n"
+        + "}\n"
+        + "xhttp.open(\"GET\", \"/computar?comando=\" + comando);\n"
+        + "xhttp.send();\n"
+        + "}\n"
+        + "</script>\n"
+        + "</body>\n"
+        + "</html>";
+    }
+}
